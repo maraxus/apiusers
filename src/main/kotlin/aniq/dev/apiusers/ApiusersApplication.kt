7d7ab7cbed90ca283389f64481af2823ce5a2f33
Rodrigo Silva <abanaunderline@hotmail.com>
2024-01-19 13:36:58 -0300

    init

---
+++ src/main/kotlin/aniq/dev/apiusers/ApiusersApplication.kt
@@ -0,0 +1,11 @@
+package aniq.dev.apiusers
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ApiusersApplication
+
+fun main(args: Array<String>) {
+	runApplication<ApiusersApplication>(*args)
+}
---
+++ src/test/kotlin/aniq/dev/apiusers/ApiusersApplicationTests.kt
@@ -0,0 +1,13 @@
+package aniq.dev.apiusers
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApiusersApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
