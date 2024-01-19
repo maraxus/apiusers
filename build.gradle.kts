e35239451ad4e472385c3a4509de0d6440b5c8d0
Rodrigo Silva <abanaunderline@hotmail.com>
2024-01-19 13:39:31 -0300

    added dependencies for logging and testing

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,12 +20,23 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.0.4")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+	// logging
+	implementation("io.github.oshai:kotlin-logging-jvm:5.1.0")
+
 	runtimeOnly("com.h2database:h2")
+//	runtimeOnly("org.postgresql:postgresql")
+
+	// testing
+	testImplementation("io.mockk:mockk:1.13.9")
+	testImplementation("com.ninja-squad:springmockk:4.0.2")
+	testImplementation("io.kotest:kotest-runner-junit5:5.8.0")
+	testImplementation("io.kotest:kotest-extensions-spring:4.4.3")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
