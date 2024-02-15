e0da330c0e43ca99ea9efac11b5f3df7e2995b20
Rodrigo Silva <abanaunderline@hotmail.com>
2024-02-15 17:12:43 -0300

    - covered more exceptions

--- src/main/kotlin/aniq/dev/apiusers/exception/GlobalExceptionHandler.kt
+++ src/main/kotlin/aniq/dev/apiusers/exception/GlobalExceptionHandler.kt
@@ -1,13 +1,15 @@
 package aniq.dev.apiusers.exception
 
-import com.fasterxml.jackson.module.kotlin.jsonMapper
 import io.github.oshai.kotlinlogging.KotlinLogging
 import org.springframework.http.*
+import org.springframework.http.converter.HttpMessageNotReadableException
+import org.springframework.orm.jpa.JpaSystemException
 import org.springframework.stereotype.Component
 import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.context.request.WebRequest
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
 
 private val logger = KotlinLogging.logger {}
@@ -26,9 +28,27 @@ class GlobalExceptionHandler: ResponseEntityExceptionHandler() {
             .map { error -> error.defaultMessage!! }
             .sorted()
         logger.info("errors: $errors")
-        val responseBody = jsonMapper().writeValueAsString(object {val errors = errors})
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).contentType(MediaType.APPLICATION_JSON).body(object {val errors = errors})
+    }
 
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).contentType(MediaType.APPLICATION_JSON).body(responseBody)
+    override fun handleHttpMessageNotReadable(
+        ex: HttpMessageNotReadableException,
+        headers: HttpHeaders,
+        status: HttpStatusCode,
+        request: WebRequest
+    ): ResponseEntity<Any>? {
+        logger.error(
+            "request=${request.getDescription(false)} --- HttpMessageNotReadableException observed  ${ex.message} - request: ${
+                request.getDescription(
+                    false
+                )
+            }", ex
+        )
+
+        val error: String = "Invalid request body"
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).contentType(MediaType.APPLICATION_JSON).body(object {
+            val errors: List<String> = listOf(error)
+        })
     }
 
     @ExceptionHandler(Exception::class)
@@ -37,9 +57,36 @@ class GlobalExceptionHandler: ResponseEntityExceptionHandler() {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(object {val errors = listOf("Oh Sh*t someone made a mess here...we're working on that")})
     }
 
+    @ExceptionHandler(MethodArgumentTypeMismatchException::class)
+    fun handleMethodArgumentTypeMismatch(
+        ex: MethodArgumentTypeMismatchException,
+        request: WebRequest
+    ): ResponseEntity<Any> {
+        logger.error(
+            "request=${request.getDescription(false)} --- MethodArgumentTypeMismatchException observed  ${ex.message} - request: ${
+                request.getDescription(
+                    false
+                )
+            }", ex
+        )
+        val error =
+            "Invalid argument type for parameter '${ex.name}'. Expected ${ex.requiredType?.simpleName}, but received ${ex.value}"
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).contentType(MediaType.APPLICATION_JSON).body(object {
+            val errors: List<String> = listOf(error)
+        })
+    }
+
     @ExceptionHandler(UserNotFoundException::class)
     fun handleNotFoundException(exception: UserNotFoundException, webRequest: WebRequest): ResponseEntity<Any> {
         logger.error("request=${webRequest.getDescription(false)} 404 not found  error=${exception.message}", exception)
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(object {val errors = listOf("${exception.message}")})
     }
+
+    @ExceptionHandler(JpaSystemException::class)
+    fun handleJpaSystemException(exception: JpaSystemException, webRequest: WebRequest): ResponseEntity<Any> {
+        logger.debug("the real cause: ${exception.mostSpecificCause}")
+        logger.error("request=${webRequest.getDescription(false)} 422 failed validating data constraints for the input error=${exception.message}", exception)
+        return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(object {val errors = listOf("${exception.message}")})
+    }
+
 }
