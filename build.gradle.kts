80faeb592f675fa4a9b8b7ecae1ee4c4c4575cc3
Rodrigo Silva <abanaunderline@hotmail.com>
2024-02-27 16:23:52 -0300

    - added some needed kotest extensions

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,8 +35,10 @@ dependencies {
 	// testing
 	testImplementation("io.mockk:mockk:1.13.9")
 	testImplementation("com.ninja-squad:springmockk:4.0.2")
-	testImplementation("io.kotest:kotest-runner-junit5:5.8.0")
-	testImplementation("io.kotest:kotest-extensions-spring:4.4.3")
+	testImplementation("io.kotest:kotest-runner-junit5:5.6.0")
+	testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.3")
+	testImplementation("io.kotest:kotest-assertions-json:5.7.2")
+	testImplementation("io.kotest:kotest-assertions-core:5.7.2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
