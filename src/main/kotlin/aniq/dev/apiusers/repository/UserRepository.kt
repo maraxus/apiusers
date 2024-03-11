def413ed3b253feee0eec57e1a46cacb5db2d27b
Rodrigo Silva <abanaunderline@hotmail.com>
2024-03-11 15:10:00 -0300

    - changed id to UUID

--- src/main/kotlin/aniq/dev/apiusers/repository/UserRepository.kt
+++ src/main/kotlin/aniq/dev/apiusers/repository/UserRepository.kt
@@ -3,6 +3,7 @@ package aniq.dev.apiusers.repository
 import aniq.dev.apiusers.entity.User
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
+import java.util.UUID
 
 @Repository
-interface UserRepositoryInterface: JpaRepository<User, Int> 
+interface UserRepository: JpaRepository<User, UUID>
